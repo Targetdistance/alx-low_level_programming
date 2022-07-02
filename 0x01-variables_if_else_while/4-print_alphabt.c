f41fc5ff3595a6957147984086191fdead6cfdf1
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-07-01 17:28:37 -0700

    created 4-print_alphabt.c file

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints out alphabets in lowercase except for q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char l[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	while (i < 26 && i != 4 && i != 14)
+	{
+		putchar(l[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
