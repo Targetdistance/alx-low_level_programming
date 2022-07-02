7a4d97a26205bb9eea0242e7194a201645f05e89
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-07-01 18:38:20 -0700

    created 5-print_numbers.c file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints out numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
