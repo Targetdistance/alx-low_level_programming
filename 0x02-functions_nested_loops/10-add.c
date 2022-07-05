ebb7cdc0371d5d74eada667db9ca25ee2a1888b3
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-07-05 16:11:34 -0700

    created 10-add.c fike

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ *
+ * @n: number one
+ * @i: number two
+ *
+ * Return: sum of the numbers
+ */
+int add(int n, int i)
+{
+	int n, i;
+	int sum;
+
+	sum = n + i;
+	return (sum);
+}
