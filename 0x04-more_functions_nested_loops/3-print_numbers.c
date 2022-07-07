62b8bf927222154aa2cb182f3eaa8e8a68910031
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-07-07 07:38:14 -0700

    created 3-print_numbers.c file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 48; j < 58; j++)
+	{
+		_putchar(j);
+	}
+	_putchar('\n');
+}
