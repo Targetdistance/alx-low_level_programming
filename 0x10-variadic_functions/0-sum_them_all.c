6633c4f8b7c9109faa1b48fa7a8cd3fc9fb97cb3
Aghogho Ken-Erhimu <aghoghoken36@gmail.com>
2022-08-04 15:33:43 -0700

    created first function with stdarg.h

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of all its parameters.
+ * @n: amount of the arguments.
+ *
+ * Return: sum of its parameters.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list a;
+	unsigned int i;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(a, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(a, int);
+
+	va_end(a);
+
+	return (sum);
+}
